cbc681b537f70d0e6ef43a54489ca30e69656663
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-12-04 22:18:20 -0300

    Array and Array 2d

---
+++ exercise/2-multidimensionalArray.js
@@ -0,0 +1,12 @@
+const europe =  ["French", "Portugal", "Spanish"]
+const africa = ["South Africa", "Angola", "Cabo Verde"];
+const america = ["Brazil", "United States", "Canada"]
+
+const countries = [
+  europe, 
+  africa, 
+  america
+];
+
+console.log(countries)
+console.log(countries[2][0])
